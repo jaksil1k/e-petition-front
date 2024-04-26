ed58c91d962a69feac99557bc6aa3fb1e2c5db16
Zhaksylyk <nartai53@gmai.com>
2024-04-27 00:11:27 +0600

    change_user layout fix

--- src/views/ChangeUserView.vue
+++ src/views/ChangeUserView.vue
@@ -5,27 +5,17 @@
     </div>
     <form @submit.prevent="onSubmit" class="change-user__form">
       <div>
-        <div class="change-user__form-div">
-          <label class="change-user__label" for="name">Full Name</label>
-          <input class="change-user__input"  type="text" id="name" v-model="user.name"/>
+        <label class="change-user__label" for="name">Full Name</label>
+        <input class="change-user__input"  type="text" id="name" v-model="user.name"/>
 
-          <label class="change-user__label" for="email">Email</label>
-          <input class="change-user__input" type="text" id="email" v-model="user.email"/>
+        <label class="change-user__label" for="iin">IIN</label>
+        <input class="change-user__input" type="text" id="iin" v-model="user.iin"/>
 
-          <label class="change-user__label" for="password">Password</label>
-          <input class="change-user__input" type="password" id="password"/>
-        </div>
+        <label class="change-user__label" for="email">Email</label>
+        <input class="change-user__input" type="text" id="email" v-model="user.email"/>
 
-        <div class="change-user__form-div">
-          <label class="change-user__label" for="iin">IIN</label>
-          <input class="change-user__input" type="text" id="iin" v-model="user.iin"/>
-
-          <label class="change-user__label" for="phone" >Phone Number</label>
-          <input class="change-user__input" type="text" id="phone" v-model="user.phone_number"/>
-
-          <label class="change-user__label" for="re-password">Confirm Password</label>
-          <input class="change-user__input" type="password" id="re-password"/>
-        </div>
+        <label class="change-user__label" for="phone" >Phone Number</label>
+        <input class="change-user__input" type="text" id="phone" v-model="user.phone_number"/>
       </div>
       <div>
         <button class="change-user__btn">Save</button>
@@ -75,7 +65,12 @@ export default {
 
 .change-user {
   height: 100vh;
-  max-width: 400px;
+
+  width: 30%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 /* - header */
@@ -84,7 +79,7 @@ export default {
   display: flex;
 
   margin-top: 4rem;
-  margin-bottom: 1.5rem;
+  margin-bottom: 2rem;
 
 }
 .change-user__header>h1 {
@@ -93,6 +88,12 @@ export default {
 
 /* - form */
 .change-user__form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 90%;
+
   border-style: solid;
   border-width: 1px;
 
@@ -100,14 +101,10 @@ export default {
 }
 
 .change-user__form>div {
-  display: flex;
-  flex-direction: row;
-}
-.change-user__form-div {
+  width: 70%;
   display: flex;
   flex-direction: column;
-  width: 50%;
-  padding: 1rem 2.5em;
+
 }
 .change-user__label {
   padding: 0.5rem 0;
@@ -117,11 +114,12 @@ export default {
   padding: 0.3rem 1rem;
   border-width: 2px;
   border-radius: 7px;
-  opacity: 60%;
+  border-color: rgba(0, 0, 0, 0.6);
+  margin: 0 1rem;
 }
 
 .change-user__btn {
-  margin: 2rem;
+  margin: 2rem 0;
   border-radius: 100px;
   border-width: 0;
   width: 8rem;
